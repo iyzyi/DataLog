b5907f73c4ef2e4d63ce39b85b7612506121c5da
iyzyi <49014178+iyzyi@users.noreply.github.com>
2021-08-10 13:38:14 +0800

    无需释放句柄即可被其他程序读取

--- DataLog/DataLog.cpp
+++ DataLog/DataLog.cpp
@@ -1,90 +1,120 @@
 #include "stdafx.h"
-#include "windows.h"
-#pragma warning(disable : 4996)
+
+#include "DataLog.h"
+//#pragma warning(disable : 4996)
 
 
-#define BUFFER_MAX_LENGTH 65536
 
 
-class CDataLog {
 
-public:
 
-	FILE *fpLog = NULL;
-	PBYTE bBuffer;
-	CRITICAL_SECTION m_cs;
+CDataLog::CDataLog(PCHAR szLogFilePath) {
+	//fpLog = _fsopen(szLogFilePath, "a+b", _SH_DENYNO);
+	//if (fpLog == NULL) {
+	//	MessageBoxA(NULL, "无法打开log文件", "错误", NULL);
+	//}
+	strcpy(this->szLogFilePath, szLogFilePath);
+	bBuffer = new BYTE[BUFFER_MAX_LENGTH];			// 千万不要写成 new BYTE(BUFFER_MAX_LENGTH);这样写会crash.
 
-	CDataLog(PCHAR szLogFilePath) {
-		fpLog = fopen(szLogFilePath, "a+b");
-		if (fpLog == NULL) {
-			MessageBoxA(NULL, "无法打开log文件", "错误", NULL);
-		}
+	//InitializeCriticalSection(&m_cs);
+}
 
-		bBuffer = new BYTE[BUFFER_MAX_LENGTH];			// 千万不要写成 new BYTE(BUFFER_MAX_LENGTH);这样写会crash.
+CDataLog::~CDataLog() {
+	//fclose(fpLog);
+	CloseLogFile();
+	delete[] bBuffer;
+	//DeleteCriticalSection(&m_cs);
+}
 
-		InitializeCriticalSection(&m_cs);
+
+VOID CDataLog::OpenLogFile() {
+	if (fpLog == NULL) {
+		fpLog = _fsopen(szLogFilePath, "a+b", _SH_DENYNO);
 	}
+}
 
-	~CDataLog() {
+
+VOID CDataLog::CloseLogFile() {
+	if (fpLog != NULL) {
 		fclose(fpLog);
-		delete[] bBuffer;
-		DeleteCriticalSection(&m_cs);
 	}
+	fpLog = NULL;
+}
 
-	VOID LogEnter() {
-		fwrite("\n", 1, 1, fpLog);
-	}
 
-	VOID LogChar(PCHAR pbOneChar) {
-		fwrite(pbOneChar, 1, 1, fpLog);
-	}
+// piblic的log函数需要OpenLogFile()和CloseLogFile()
 
-	VOID LogChar(CHAR cData) {
-		sprintf((PCHAR)bBuffer, "%c", cData);
-		fwrite((PCHAR)bBuffer, 1, 1, fpLog);
-	}
+VOID CDataLog::LogString(PCHAR szData) {
+	//EnterCriticalSection(&m_cs);
+	OpenLogFile();
+	fwrite(szData, strlen(szData), 1, fpLog);
+	CloseLogFile();
+	//LeaveCriticalSection(&m_cs);
+}
 
-	VOID LogString(PCHAR szData) {
-		EnterCriticalSection(&m_cs);
-		fwrite(szData, strlen(szData), 1, fpLog);
-		LeaveCriticalSection(&m_cs);
-	}
 
-	VOID LogHexString(PCHAR szPreString, PBYTE pbData, DWORD dwDataLen) {
-		EnterCriticalSection(&m_cs);
+VOID CDataLog::LogHexData(PCHAR szPreString, PBYTE pbData, DWORD dwDataLen) {
+	//EnterCriticalSection(&m_cs);
+	OpenLogFile();
 
-		fwrite(szPreString, strlen(szPreString), 1, fpLog);
+	fwrite(szPreString, strlen(szPreString), 1, fpLog);
 
-		DWORD dwRow = 0, dwColumn = 0;
-		for (dwRow = 0; dwRow < dwDataLen / 16 + 1; dwRow++) {
-			for (dwColumn = 0; (dwRow * 16 + dwColumn < dwDataLen) && (dwColumn < 16); dwColumn++) {
-				sprintf((PCHAR)bBuffer, "0x%02x ", pbData[dwRow * 16 + dwColumn]);
-				LogString((PCHAR)bBuffer);
-			}
+	//system("pause");
 
-			if (dwColumn != 16) {
-				while (dwColumn < 16) {
-					LogString("     ");
-					dwColumn++;
-				}
-			}
-			LogChar("\t");
-
-			for (dwColumn = 0; (dwRow * 16 + dwColumn < dwDataLen) && (dwColumn < 16); dwColumn++) {
-				DWORD dwIndex = dwRow * 16 + dwColumn;
-				if (pbData[dwIndex] >= 32 && pbData[dwIndex] <= 126) {
-					LogChar(pbData[dwIndex]);
-				}
-				else {
-					LogChar(".");
-				}
-			}
-			LogEnter();
+	DWORD dwRow = 0, dwColumn = 0;
+	for (dwRow = 0; dwRow < dwDataLen / 16 + 1; dwRow++) {
+		for (dwColumn = 0; (dwRow * 16 + dwColumn < dwDataLen) && (dwColumn < 16); dwColumn++) {
+			sprintf((PCHAR)bBuffer, "0x%02x ", pbData[dwRow * 16 + dwColumn]);
+			__LogString((PCHAR)bBuffer);
 		}
 
-		LeaveCriticalSection(&m_cs);
+		if (dwColumn != 16) {
+			while (dwColumn < 16) {
+				__LogString("     ");
+				dwColumn++;
+			}
+		}
+		__LogChar("\t");
+
+		for (dwColumn = 0; (dwRow * 16 + dwColumn < dwDataLen) && (dwColumn < 16); dwColumn++) {
+			DWORD dwIndex = dwRow * 16 + dwColumn;
+			if (pbData[dwIndex] >= 32 && pbData[dwIndex] <= 126) {
+				__LogChar(pbData[dwIndex]);
+			}
+			else {
+				__LogChar(".");
+			}
+		}
+		__LogEnter();
 	}
-};
+
+	CloseLogFile();
+	//LeaveCriticalSection(&m_cs);
+}
+
+
+
+// private的log函数不能OpenLogFile()和CloseLogFile()
+
+VOID CDataLog::__LogString(PCHAR szData) {
+	fwrite(szData, strlen(szData), 1, fpLog);
+}
+
+
+VOID CDataLog::__LogEnter() {
+	fwrite("\n", 1, 1, fpLog);
+}
+
+
+VOID CDataLog::CDataLog::__LogChar(PCHAR pbOneChar) {
+	fwrite(pbOneChar, 1, 1, fpLog);
+}
+
+
+VOID CDataLog::__LogChar(CHAR cData) {
+	sprintf((PCHAR)bBuffer, "%c", cData);
+	fwrite((PCHAR)bBuffer, 1, 1, fpLog);
+}
 
 
 
@@ -97,12 +127,13 @@ int main()
 	sprintf((PCHAR)m_DataLog.bBuffer, "我的数据是%d，你的数据是0x%llx\n", 123, 0x123456789abc);
 	m_DataLog.LogString((PCHAR)m_DataLog.bBuffer);
 
+	system("pause");
 
 	CHAR szPreString[] = "接下来的数据是十六进制形式：\n";
 	CHAR szData[] = "对于互斥锁我们要先知道为什么要用互斥锁？它能解决什么问题？根据这两个问题，可以来举个例子说明一下，假如现在我们要求1 - 10000的和，然后我们为了提高效率，我们建立两个线程同时去计算[1, 5000)的和以及[5000, 10001)的和，那么用于计算和的变量都用相同的ans来获取结果，代码如下";
 	DWORD dwDataLen = strlen(szData);
-	m_DataLog.LogHexString(szPreString, (PBYTE)szData, dwDataLen);
+	m_DataLog.LogHexData(szPreString, (PBYTE)szData, dwDataLen);
 
-
+	
     return 0;
 }
---
+++ DataLog/DataLog.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "stdafx.h"
+#include "windows.h"
+#pragma warning(disable : 4996)
+
+
+#define BUFFER_MAX_LENGTH 65536
+
+
+class CDataLog {
+
+public:
+	PBYTE bBuffer;
+	FILE* fpLog = NULL;
+
+	CDataLog(PCHAR szLogFilePath);
+	~CDataLog();
+
+	VOID LogString(PCHAR szData);
+	VOID LogHexData(PCHAR szPreString, PBYTE pbData, DWORD dwDataLen);
+
+
+private:
+	//FILE *fpLog = NULL;
+	//CRITICAL_SECTION m_cs;
+	CHAR szLogFilePath[MAX_PATH];
+
+	VOID OpenLogFile();
+	VOID CloseLogFile();
+
+	VOID __LogString(PCHAR szData);
+	VOID __LogEnter();
+	VOID __LogChar(PCHAR pbOneChar);
+	VOID __LogChar(CHAR cData);
+};
